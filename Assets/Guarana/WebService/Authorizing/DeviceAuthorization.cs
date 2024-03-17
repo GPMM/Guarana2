2165709997d74b716f071a63ee73e03f4b5ab9cc
Joel dos Santos <jsantos@eic.cefet-rj.br>
2024-03-16 23:00:38 -0300

    WebService: authorizing step

---
+++ Assets/Guarana/WebService/Authorizing/DeviceAuthorization.cs
@@ -0,0 +1,49 @@
+using System;
+
+public class DeviceAuthorization : WSStep
+{
+    private string clientId = Guid.NewGuid().ToString();
+
+    void Start()
+    {
+        StartPairing();
+    }
+
+    
+    void Update()
+    {
+        if (running) return;
+
+        if (output != null)
+        {
+            Stop();
+        }
+    }
+
+
+    private void StartPairing()
+    {
+        running = true;
+
+        // First get the user authorization according to the pairing method
+        StartCoroutine(dtv.GetAuthorize(clientId, "Guarana Player", CompletePairing));
+    }
+
+    private void CompletePairing(DTVAuthorizeReturn pairing)
+    {
+        // Get the accessToken
+        StartCoroutine(dtv.GetTokenWithChallenge(clientId, pairing.challenge, SetupToken));
+    }
+
+
+    private void SetupToken(DTVTokenReturn token)
+    {
+        dtv.SetAccessToken(token.accessToken);
+        dtv.SetRefreshToken(token.refreshToken);
+
+        output = new WSStepIO();
+        output.status = WSStepStatus.OK;
+
+        running = false;
+    }
+}
