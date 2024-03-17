21decbc5ab2f20be08f2f1a1c777f029d5fd866a
Joel dos Santos <jsantos@eic.cefet-rj.br>
2024-03-17 13:11:04 -0300

    WebService: remaining states and webservice managing

---
+++ Assets/Guarana/WebService/Failed/Failed.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Failed : WSStep
+{
+
+    void Start()
+    {
+        Debug.Log("I've failed!");
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Guarana/WebService/Halted/Halted.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Halted : WSStep
+{
+    
+    void Start()
+    {
+        Debug.Log("I'm halted!");
+    }
+
+    
+    void Update()
+    {
+
+    }
+}
