4768eb9751c9faa31c220e1607ebf077d1572ea4
Joel dos Santos <jsantos@eic.cefet-rj.br>
2024-03-16 23:36:31 -0300

    WebService: websocket connection step

---
+++ Assets/Guarana/WebService/Connecting/ConnectWebSocket.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using WebSocketSharp;
+
+public class ConnectWebSocket : WSStep
+{
+    void Start()
+    {
+        Connect();
+    }
+
+    
+    void Update()
+    {
+        if (running) return;
+
+        if (output != null)
+        {
+            Stop();
+        }
+    }
+
+
+    private void Connect()
+    {
+        running = true;
+
+        WebSocket ws = new WebSocket(((RegisterOutput)input).url);
+        ws.OnOpen += (sender, e) =>
+        {
+            if (!gameObject.activeInHierarchy)
+                return;
+
+            output = new ConnectOutput();
+            output.status = WSStepStatus.OK;
+            ((ConnectOutput)output).handle = ((RegisterOutput)input).handle;
+            ((ConnectOutput)output).ws = ws;
+            running = false;
+        };
+
+        ws.OnError += (sender, e) =>
+        {
+            if (!gameObject.activeInHierarchy)
+                return;
+
+            Debug.Log(e.Message);
+            output = new WSStepIO();
+            output.status = WSStepStatus.ERROR;
+            running = false;
+        };
+
+        ws.Connect();
+    }
+}
