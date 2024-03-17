9503963dd450c2d76c7d7b2523efc3ba56413294
Joel dos Santos <jsantos@eic.cefet-rj.br>
2024-03-16 23:27:09 -0300

    WebService: steps to register and unregisted a device

---
+++ Assets/Guarana/WebService/Registering/RegisterDevice.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+
+public class RegisterDevice : WSStep
+{
+    private DTVRemoteDeviceBody msgBody;
+
+
+    void Start()
+    {
+        msgBody = new DTVRemoteDeviceBody();
+        msgBody.deviceClass = "Guarana";
+
+        msgBody.supportedFormats = new List<string>();
+        msgBody.supportedFormats.Add("application/x-ncl360");
+
+        msgBody.recognizableEvents = new List<string>();
+        msgBody.recognizableEvents.Add("view");
+
+        running = true;
+        StartCoroutine(dtv.PostRemoteDevice(msgBody, CompleteRegister));
+    }
+
+    
+    void Update()
+    {
+        if (running) return;
+
+        if (output != null)
+        {
+            Stop();
+        }
+    }
+
+
+    private void CompleteRegister(DTVRemoteDeviceReturn result)
+    {
+        output = new RegisterOutput();
+        ((RegisterOutput)output).handle = result.handle;
+        ((RegisterOutput)output).url = result.url;
+
+        if (result.handle == null)
+        {
+            output.status = WSStepStatus.ERROR;
+        }
+        else
+        {
+            output.status = WSStepStatus.OK;
+        }
+
+        running = false;
+    }
+}
---
+++ Assets/Guarana/WebService/Unregistering/UnregisterDevice.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+public class UnregisterDevice : WSStep
+{
+    
+    void Start()
+    {
+        running = true;
+
+        string handle = ((UnregisteringInput)input).handle;
+        StartCoroutine(dtv.DeleteRemoteDevice(handle, CompleteUnregister));
+    }
+
+    
+    void Update()
+    {
+        if (running) return;
+
+        if (output != null)
+        {
+            Stop();
+        }
+    }
+
+
+    private void CompleteUnregister(APIMessageBody result)
+    {
+        output = new WSStepIO();
+        output.status = WSStepStatus.OK;
+
+        running = false;
+    }
+}
